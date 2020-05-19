3353197b0ba1a9098b55135acbb34c1a2576d421
Piotr Żyziński <33286821+x296@users.noreply.github.com>
2020-05-19 18:49:36 +0200

    finally finished / i think

--- 7youtubeVideosDownloader/youtube_downloader.py
+++ 7youtubeVideosDownloader/youtube_downloader.py
@@ -6,6 +6,7 @@ from PIL import ImageTk, Image
 import urllib.parse
 import io
 import os
+import emoji
 
 class Application(tk.Frame):
 	# Main initialization whole app
@@ -13,9 +14,17 @@ class Application(tk.Frame):
 		super().__init__(master)
 		self.master = master
 		self.master.title('YouTube Downloader')
-		self.master.geometry('500x400')
+		# self.master.geometry('400x250')
+		self.master.resizable(height=0, width=0)
 		self.pack(fill=tk.BOTH)
-		self.create_instruction_label()
+		
+		self.row_index = 0
+		self.is_loaded = False
+
+		tk.Grid.grid_columnconfigure(self, 0, weight=1)
+		tk.Grid.grid_rowconfigure(self, 0, weight=1)
+
+		# self.create_instruction_label()
 		self.create_ytLink_textbox()
 		self.create_load_button()
 		self.create_quit_button()
@@ -27,46 +36,59 @@ class Application(tk.Frame):
 		self.instruction_lbl = tk.Label(self, 
 										text='''Hey,\npaste YouTube video URL below, smash LOAD button,\nclick DOWNLOAD button near resolution you are interested in\nand select directory to save file.''',
 										justify=tk.LEFT)
-		self.instruction_lbl.grid(row=0, column=0, columnspan=3)
+		self.instruction_lbl.grid(row=self.row_index, column=0, columnspan=3)
+		self.row_index += 1
 
 	def create_ytLink_textbox(self):
-		self.ytLink_txtbx = tk.Entry(self, fg='grey')
+		self.ytLink_txtbx = tk.Entry(self, fg='grey', width=23)
 		self.ytLink_txtbx.insert(0, 'paste YouTube video URL here...')
 		self.ytLink_txtbx.bind('<FocusIn>', self.insert_URL)
 		self.ytLink_txtbx.bind('<Return>', self.load_URL)
-		self.ytLink_txtbx.grid(row=1, column=0, columnspan=2)
+		self.ytLink_txtbx.grid(row=self.row_index, column=0, columnspan=3, sticky=tk.E+tk.W)
+		self.row_index += 1
 
 	def create_load_button(self):
 		self.load_btn = tk.Button(self, text='LOAD', width=10, command=self.load_URL)
 		self.load_btn.bind('<Return>', self.load_URL)
-		self.load_btn.grid(row=1, column=2)
+		self.load_btn.grid(row=self.row_index, column=0, columnspan=2, sticky=tk.E+tk.W)
 
 	def create_ytVideo_title_label(self):
-		self.ytVideo_title_lbl = tk.Label(self, text=self.ytFile.title)
-		self.ytVideo_title_lbl.grid(row=2, column=0, columnspan=3)
+		clean_title= ''
+		for letter in self.ytFile.title:	
+			if letter in emoji.UNICODE_EMOJI:
+				continue
+			clean_title += letter
+		self.ytVideo_title_lbl = tk.Label(self, text=clean_title, wraplength=330)
+		self.ytVideo_title_lbl.grid(row=self.row_index, column=0, columnspan=3, pady=10)
+		self.row_index += 1
 
-	def create_download_label(self, i, value):
+	def create_download_label(self, value):
 		self.download_lbl = tk.Label(self, text=value)
-		self.download_lbl.grid(row=i, column=1)
+		self.download_lbl.grid(row=self.row_index, column=1)
 
 	def create_video_download_button(self, i):
-		self.download_btn = tk.Button(self, text='DOWNLOAD', fg='DodgerBlue2', width=10, command=lambda: self.download_video(i-3))
-		self.download_btn.grid(row=i, column=2)
+		self.download_btn = tk.Button(self, text='DOWNLOAD', fg='DodgerBlue2', width=10, command=lambda: self.download_video(i))
+		self.download_btn.bind('<Return>', lambda *event: self.download_video(i))
+		self.download_btn.grid(row=self.row_index, column=2)
+		self.row_index += 1
 
-	def create_audio_download_button(self, i):
-		self.download_btn = tk.Button(self, text='DOWNLOAD', fg='DodgerBlue2', width=10, command=lambda: self.download_audio(i-3))
-		self.download_btn.grid(row=i, column=2)
+	def create_audio_download_button(self):
+		self.download_btn = tk.Button(self, text='DOWNLOAD', fg='DodgerBlue2', width=10, command=lambda: self.download_audio())
+		self.download_btn.bind('<Return>', lambda *event: self.download_audio())
+		self.download_btn.grid(row=self.row_index, column=2)
+		self.row_index += 1
 
 	def create_ytVideo_thumbnail_label(self, rowspan):
 		image = self.generate_image(240, 135, self.ytFile.thumbnail_url)
 		self.ytVideo_thumbnail_lbl = tk.Label(self, image=image)
 		self.ytVideo_thumbnail_lbl.image = image
-		self.ytVideo_thumbnail_lbl.grid(row=3, column=0, rowspan=rowspan)
+		self.ytVideo_thumbnail_lbl.grid(row=self.row_index, column=0, rowspan=rowspan)
 
 	def create_quit_button(self):
 		self.quit_btn = tk.Button(self, width=10, text='QUIT', fg='red', command=self.quit_app)
 		self.quit_btn.bind('<Return>', self.quit_app)
-		self.quit_btn.grid(row=10, column=2)
+		self.quit_btn.grid(row=self.row_index, column=2)
+		self.row_index += 1
 
 	# Creating funcs for widgets
 	def insert_URL(self, event):
@@ -74,27 +96,36 @@ class Application(tk.Frame):
 		self.ytLink_txtbx['fg'] = 'black'
 
 	def load_URL(self, *event):
-		self.URL = self.ytLink_txtbx.get()
-		self.ytFile = YouTube(self.URL)
-		self.create_ytVideo_title_label()
-		i = 0
-		for i, stream in zip(range(len(self.ytFile.streams.filter(adaptive=True, mime_type='video/mp4'))), reversed(self.ytFile.streams.filter(adaptive=True, mime_type='video/mp4').order_by('resolution'))):
-			self.create_download_label(i+3, stream.resolution)
-			self.create_video_download_button(i+3)
-		self.create_download_label(i+4, 'MP3')
-		self.create_audio_download_button(i+4)
-		self.create_ytVideo_thumbnail_label(i+2)
+		if not(self.is_loaded):
+			self.URL = self.ytLink_txtbx.get()
+			self.ytFile = YouTube(self.URL)
+			self.create_ytVideo_title_label()
+			self.create_ytVideo_thumbnail_label(3)
+			i = 0
+			for i, stream in zip(range(len(self.ytFile.streams.filter(adaptive=True, mime_type='video/mp4', fps=30))), reversed(self.ytFile.streams.filter(adaptive=True, mime_type='video/mp4', fps=30).order_by('resolution'))):
+				self.create_download_label(stream.resolution)
+				self.create_video_download_button(i)
+			self.create_download_label('MP3')
+			self.create_audio_download_button()
+			self.ytVideo_thumbnail_lbl.grid(rowspan=i+3)
+			self.is_loaded = True
+		else:
+			for i, widget in zip(range((self.row_index-2)*2), self.grid_slaves()):
+				widget.grid_forget()
+			self.is_loaded = False
+			self.row_index = 2
+			self.load_URL()
 
-	def download_video(self, i):
+	def download_video(self, i, *event):
 		path_filename = self.save_file('.mp4')
 		path = path_filename[:path_filename.rfind('/')]
 		filename = path_filename[path_filename.rfind('/')+1:]
-		self.ytFile.streams.filter(adaptive=True, mime_type='video/mp4').order_by('resolution')[-1-i].download(output_path=path, filename='video')
+		self.ytFile.streams.filter(adaptive=True, mime_type='video/mp4', fps=30).order_by('resolution')[-1-i].download(output_path=path, filename='video')
 		self.ytFile.streams.filter(adaptive=True, mime_type='audio/mp4').order_by('abr')[-1].download(output_path=path, filename='audio')
 		os.system('ffmpeg -i {} -i {} -c copy {}'.format(path+'/video.mp4', path+'/audio.mp4', path+'/'+self.creating_output_filename(filename)))
 		os.system('rm {} {}'.format(path+'/video.mp4', path+'/audio.mp4'))
 
-	def download_audio(self, i):
+	def download_audio(self, *event):
 		path_filename = self.save_file('.mp3')
 		path = path_filename[:path_filename.rfind('/')]
 		filename = path_filename[path_filename.rfind('/')+1:]
