1b133214fee4eba09d1d0987e37cc3cf8ce9b4e8
Piotr Żyziński <33286821+x296@users.noreply.github.com>
2020-06-04 07:42:06 +0200

    first version, working engine, no possibility to copy password, no layout

---
+++ 11randomPasswordGenerator/password_geneator.py
@@ -0,0 +1,93 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+import secrets
+import string
+
+class Application(tk.Frame):
+	def __init__(self, master=None):
+		super().__init__(master)
+		self.master = master
+		self.master.title('Password Generator')
+		self.master.resizable(height=0, width=0)
+		self.pack(fill=tk.BOTH)
+
+		self.create_instruction_label()
+		self.create_checkbuttons()
+		self.create_combobox()
+		self.create_password_label()
+		self.create_button()
+
+	def create_instruction_label(self):
+		self.int_lbl = tk.Label(self, text='Select, which and how many characters \nYou want in your super-safe password.')
+		self.int_lbl.pack()
+
+	def create_checkbuttons(self):
+		self.password_options = {
+			'lowercase': tk.IntVar(),
+			'uppercase': tk.IntVar(),
+			'digit': tk.IntVar(),
+			'punctuation': tk.IntVar()
+		}
+		for option in self.password_options:
+			self.chkbtn = 'checkbutton ' + option
+			self.chkbtn = tk.Checkbutton(self, text=option, variable=self.password_options[option])
+			self.chkbtn.select()
+			self.chkbtn.pack()
+	
+	def create_combobox(self):
+		pass_len = []
+		for i in range(4, 21):
+			pass_len.append(i)
+
+		self.password_length = tk.IntVar(self)
+		self.password_length.set(4) # initial value
+
+		option = tk.OptionMenu(self, self.password_length, *pass_len)
+		option.pack()
+
+	def create_password_label(self):
+		self.your_password = tk.StringVar()
+		self.your_password.set('Your password')
+		self.pass_lbl = tk.Label(self, textvariable=self.your_password)
+		self.pass_lbl.pack()
+
+	def create_button(self):
+		self.btn = tk.Button(self, text='btn', command=self.generate_password)
+		self.btn.pack()
+
+	def generate_password(self):
+		characters_bank = ''
+		password = ''
+		
+		if self.password_options['lowercase'].get() == 1:
+			password += secrets.choice(string.ascii_lowercase)
+			characters_bank += string.ascii_lowercase
+		if self.password_options['uppercase'].get() == 1:
+			password += secrets.choice(string.ascii_uppercase)
+			characters_bank += string.ascii_uppercase
+		if self.password_options['digit'].get() == 1:
+			password += secrets.choice(string.digits)
+			characters_bank += string.digits
+		if self.password_options['punctuation'].get() == 1:
+			password += secrets.choice(string.punctuation)
+			characters_bank += string.punctuation
+
+		for i in range(int(self.password_length.get())-len(password)):
+			password += secrets.choice(characters_bank)
+
+		char_list = list(password)
+		secrets.SystemRandom().shuffle(char_list)
+
+		password = ''.join(char_list)
+
+		self.your_password.set(password)
+
+
+
+def main():
+	root = tk.Tk()
+	app = Application(master=root)
+	app.mainloop()
+
+if __name__ == '__main__':
+	main()
