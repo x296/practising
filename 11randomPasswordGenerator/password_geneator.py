6799957102a3296ec477f492eba67f2bde5efe74
Piotr Żyziński <33286821+x296@users.noreply.github.com>
2020-06-05 00:33:21 +0200

    finished app, with coping function

--- 11randomPasswordGenerator/password_geneator.py
+++ 11randomPasswordGenerator/password_geneator.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+import os
 import secrets
 import string
 
@@ -13,51 +14,61 @@ class Application(tk.Frame):
 
 		self.create_instruction_label()
 		self.create_checkbuttons()
-		self.create_combobox()
+		self.create_optionMenu()
+		self.create_generate_button()
 		self.create_password_label()
-		self.create_button()
+		self.create_copy_button()
 
 	def create_instruction_label(self):
 		self.int_lbl = tk.Label(self, text='Select, which and how many characters \nYou want in your super-safe password.')
-		self.int_lbl.pack()
+		self.int_lbl.grid(row=0, column=0, columnspan=2)
 
 	def create_checkbuttons(self):
 		self.password_options = {
-			'lowercase': tk.IntVar(),
 			'uppercase': tk.IntVar(),
 			'digit': tk.IntVar(),
+			'lowercase': tk.IntVar(),
 			'punctuation': tk.IntVar()
 		}
-		for option in self.password_options:
+		for i, option in zip(range(len(self.password_options.items())), self.password_options):
 			self.chkbtn = 'checkbutton ' + option
-			self.chkbtn = tk.Checkbutton(self, text=option, variable=self.password_options[option])
+			self.chkbtn = tk.Checkbutton(self, text=option, anchor=tk.W, variable=self.password_options[option])
 			self.chkbtn.select()
-			self.chkbtn.pack()
+			if i%4 < 2:
+				self.chkbtn.grid(row=(i+1)%2+1, column=i%2, sticky=tk.W)
+			else:
+				self.chkbtn.grid(row=i%2+1, column=i%2, sticky=tk.W)
 	
-	def create_combobox(self):
+	def create_optionMenu(self):
 		pass_len = []
 		for i in range(4, 21):
 			pass_len.append(i)
 
-		self.password_length = tk.IntVar(self)
-		self.password_length.set(4) # initial value
+		self.password_length = tk.StringVar(self)
+		self.password_length.set('length') # initial value
 
 		option = tk.OptionMenu(self, self.password_length, *pass_len)
-		option.pack()
+		option.grid(row=3, column=0, sticky=tk.W+tk.E)
 
 	def create_password_label(self):
 		self.your_password = tk.StringVar()
 		self.your_password.set('Your password')
-		self.pass_lbl = tk.Label(self, textvariable=self.your_password)
-		self.pass_lbl.pack()
+		self.pass_lbl = tk.Label(self, font=('Helvetica', '14', 'bold'), width=30, textvariable=self.your_password)
+		self.pass_lbl.grid(row=5, column=0)
 
-	def create_button(self):
-		self.btn = tk.Button(self, text='btn', command=self.generate_password)
-		self.btn.pack()
+	def create_generate_button(self):
+		self.gen_btn = tk.Button(self, text='Generate', fg='red', command=self.generate_password)
+		self.gen_btn.grid(row=3, column=1, sticky=tk.W+tk.E)
+
+	def create_copy_button(self):
+		self.cpy_btn = tk.Button(self, text="Copy", fg='blue', command=self.copy_password)
+		self.cpy_btn.grid(row=5, column=1, sticky=tk.W+tk.E)
 
 	def generate_password(self):
 		characters_bank = ''
 		password = ''
+		if self.password_length.get() == 'length':
+			self.password_length.set(8)
 		
 		if self.password_options['lowercase'].get() == 1:
 			password += secrets.choice(string.ascii_lowercase)
@@ -82,6 +93,9 @@ class Application(tk.Frame):
 
 		self.your_password.set(password)
 
+	def copy_password(self):
+		command = 'echo ' + self.your_password.get().strip() + ' | pbcopy'
+		os.system(command)
 
 
 def main():
