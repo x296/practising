11bf7e07af24ba6140b1d21bd4773c8522d8b01b
Piotr Żyziński <33286821+x296@users.noreply.github.com>
2020-06-18 00:13:46 +0200

    added some extra features based on tuts

--- 26simpleBanikngApp/MyClass.java
+++ 26simpleBanikngApp/MyClass.java
@@ -1,49 +1,70 @@
-// import java.util.Scanner;
-import java.util.*;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MyClass {
 	public static void main(String[] args) {
-		BankAccount obj1 = new BankAccount("XYZ", "BA001");
-		BankAccount obj2 = new BankAccount("ABC", "BA002");
-
-		obj1.showMenu();
+		BankAccount account = new BankAccount("Jan Kowalski", 1);
+		account.showMenu();
 	}
 }
 
 class BankAccount {
 	int balance;
-	int previousTransaction;
+	String previousTransaction;
 	String customerName;
-	String customerId;
+	int customerId;
+	List<String> transactionHistory = new ArrayList<String>();
 
-	BankAccount(String cname, String cid){
+	BankAccount(String cname, int cid) {
 		customerName = cname;
 		customerId = cid;
+	}
+
+	void setName(String cname) {
+		customerName = cname;
+		transactionHistory.add("Name changed to "+cname);
 	}
 
 	void deposit(int amount) {
 		if(amount != 0) {
 			balance = balance + amount;
-			previousTransaction = amount;
+			String forPrevioustransaction = Integer.toString(amount);
+			previousTransaction = "Deposited: "+forPrevioustransaction;
+			transactionHistory.add(previousTransaction);
 		}
 	}
 
 	void withdraw(int amount) {
 		if(amount != 0) {
 			balance = balance - amount;
-			previousTransaction = -amount;
+			String forPrevioustransaction = Integer.toString(amount);
+			previousTransaction = "Withdrawn: "+forPrevioustransaction;
+			transactionHistory.add(previousTransaction);
+		}
+	}
+
+	void donate(int amount, String charity) {
+		if(amount != 0) {
+			balance = balance - amount;
+			String forPrevioustransaction = Integer.toString(amount);
+			previousTransaction = "Donated "+forPrevioustransaction+" for "+charity;
+			transactionHistory.add(previousTransaction);
 		}
 	}
 
 	void getPreviousTransaction() {
-		if(previousTransaction > 0) {
-			System.out.println("Deposited: "+previousTransaction);
-		}
-		else if (previousTransaction < 0) {
-			System.out.println("Withdrawn: "+Math.abs(previousTransaction));
+		if(previousTransaction != null) {
+			System.out.println(previousTransaction);
 		}
 		else {
 			System.out.println("No transaction occured.");
+		}
+	}
+
+	void getHistoryOfTransactions() {
+		for(int i=0; i<transactionHistory.size(); i++) {
+			System.out.println(transactionHistory.get(i));
 		}
 	}
 
@@ -51,14 +72,17 @@ class BankAccount {
 		char option = '\0';
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.println(" Welcome "+customerName);
-		System.out.println("Your ID is "+customerId);
+		System.out.println("Welcome, Your ID is "+customerId);
 		System.out.println("\n");
-		System.out.println("A. Check Balance");
-		System.out.println("B. Deposit");
-		System.out.println("C. Withdraw");
-		System.out.println("D. Previous transaction");
-		System.out.println("E. Exit");
+		System.out.println("A. Set customer name");
+		System.out.println("B. View customer name");
+		System.out.println("C. Check Balance");
+		System.out.println("D. Deposit");
+		System.out.println("E. Withdraw");
+		System.out.println("F. Donate");
+		System.out.println("G. Previous transaction");
+		System.out.println("H. Show history");
+		System.out.println("I. Exit");
 
 		do {
 			System.out.println("***********************************************");
@@ -70,12 +94,29 @@ class BankAccount {
 			switch(option) {
 				case 'A':
 					System.out.println("-----------------------------------------------");
+					System.out.println("Enter customer name: ");
+					System.out.println("-----------------------------------------------");
+					scanner.nextLine();
+					String newName = scanner.nextLine();
+					setName(newName);
+					System.out.println("\n");
+					break;
+
+				case 'B':
+					System.out.println("-----------------------------------------------");
+					System.out.println("Your name is: "+customerName);
+					System.out.println("-----------------------------------------------");
+					System.out.println("\n");
+					break;
+
+				case 'C':
+					System.out.println("-----------------------------------------------");
 					System.out.println("Balance = "+balance);
 					System.out.println("-----------------------------------------------");
 					System.out.println("\n");
 					break;
 
-				case 'B':
+				case 'D':
 					System.out.println("-----------------------------------------------");
 					System.out.println("Enter an amount to deposit: ");
 					System.out.println("-----------------------------------------------");
@@ -84,7 +125,7 @@ class BankAccount {
 					System.out.println("\n");
 					break;
 
-				case 'C':
+				case 'E':
 					System.out.println("-----------------------------------------------");
 					System.out.println("Enter an amount to withdraw: ");
 					System.out.println("-----------------------------------------------");
@@ -93,21 +134,43 @@ class BankAccount {
 					System.out.println("\n");
 					break;
 
-				case 'D':
+				case 'F':
+					System.out.println("-----------------------------------------------");
+					System.out.println("Enter amount of donation: ");
+					System.out.println("-----------------------------------------------");
+					int amount3 = scanner.nextInt();
+					System.out.println("-----------------------------------------------");
+					System.out.println("Enter name of charity, which you want bestow: ");
+					System.out.println("-----------------------------------------------");
+					scanner.nextLine();
+					String charname = scanner.nextLine();
+					donate(amount3, charname);
+					System.out.println("\n");
+					break;
+
+				case 'G':
 					System.out.println("-----------------------------------------------");
 					getPreviousTransaction();
 					System.out.println("-----------------------------------------------");
 					System.out.println("\n");
 					break;
 
-				case 'E':
+				case 'H':
+					System.out.println("-----------------------------------------------");
+					System.out.println("History of operations: ");
+					System.out.println("-----------------------------------------------");
+					getHistoryOfTransactions();
+					System.out.println("\n");
+					break;
+
+				case 'I':
 					System.out.println("***********************************************");
 					break;
 
 				default:
 					System.out.println("Invalid Option! Please enter again.");
 			}
-		} while(option != 'E');
+		} while(option != 'I');
 		System.out.println("Thank You for using our services!");
 	}
 }
